00f763b928f62704e76541b5e02d3d8010246245
jzhmcoo1 <jzhmzlxcoo1@gmail.com>
2021-11-28 11:02:25 +0800

    feat: 增加search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,10 @@ const config = {
             position: "right",
             className: "header-github-link",
           },
+          {
+            type: "search",
+            position: "right",
+          },
         ],
       },
       footer: {
@@ -144,6 +148,26 @@ const config = {
       },
       colorMode: {
         respectPrefersColorScheme: true,
+      },
+      algolia: {
+        // If Algolia did not provide you any appId, use 'BH4D9OD16A'
+        appId: "ZNUM42B70I",
+
+        // Public API key: it is safe to commit it
+        apiKey: "f53a05d9fa1237b3ecf063dbb4ffc16e",
+
+        indexName: "blog_lihangzhu",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        //... other Algolia params
       },
     }),
 };
